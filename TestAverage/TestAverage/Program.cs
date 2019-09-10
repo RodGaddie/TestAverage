6ad4d282c6875a7bfbeb61ccc146a8f4570d23b5
Rod Gaddie <rodgaddie@gmail.com>
2019-09-10 11:02:11 -0500

    final commit

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -6,7 +6,27 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first test score");
+
+            var responseFromUser1 = int.Parse (Console.ReadLine());
+
+            Console.WriteLine("What is your second test score");
+
+            var responseFromUser2 = int.Parse (Console.ReadLine());
+
+            Console.WriteLine("What is your third test score");
+
+            var responseFromUser3 = int.Parse (Console.ReadLine());
+
+            var TestAverage = (responseFromUser1 + responseFromUser2 + responseFromUser3) /3;
+
+             Console.WriteLine($"your test average is {TestAverage}");
+
+
+
+
+
+                
         }
     }
 }
